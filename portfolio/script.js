7f08970c6145ca5a4fc39c622e62f3c6d9411419
Viggiii <vigneshmuthaiah62@gmail.com>
2024-01-16 19:38:46 +0530

    update

--- portfolio/script.js
+++ portfolio/script.js
@@ -6,30 +6,13 @@ btns.forEach(function(buttons){
     buttons.addEventListener("click", function(){
         console.log(buttons.attributes.id.value);
 
-        if(buttons.attributes.id.value == "ui"){
-            document.getElementById("uiDesign").style.display="block";
-            document.getElementById("webDesign").style.display="none";
-            document.getElementById("android").style.display="none";
-
-        }
         if(buttons.attributes.id.value == "web"){
             document.getElementById("uiDesign").style.display="none";
             document.getElementById("webDesign").style.display="block";
             document.getElementById("android").style.display="none";
 
         }
-        if(buttons.attributes.id.value == "mobile"){
-            document.getElementById("uiDesign").style.display="none";
-            document.getElementById("webDesign").style.display="none";
-            document.getElementById("android").style.display="block";
-
-        }
-        if(buttons.attributes.id.value == "all"){
-            document.getElementById("uiDesign").style.display="block";
-            document.getElementById("webDesign").style.display="block";
-            document.getElementById("android").style.display="block";
-
-        }
+        
     })
 })
 
